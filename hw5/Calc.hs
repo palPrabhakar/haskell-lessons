4a6e8e85232ffb4d518dc864e3781c8a6d0f0a7c
Prabhakar Pal <prabhakar.pal00@gmail.com>
2023-04-23 15:33:21 -0400

    added evalStr

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -1,6 +1,7 @@
 module Calc where
 
 import ExprT
+import Parser
 
 -- | evaluate expression
 --
@@ -12,3 +13,22 @@ eval :: ExprT -> Integer
 eval (Lit num) = num
 eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2
+
+
+-- | evaluate strings
+--
+-- Examples:
+--
+-- >>> evalStr "1"
+-- Just 1
+--
+-- >>> evalStr "(2+3)*4"
+-- Just 20
+--
+-- >>> evalStr "2+3*4"
+-- Just 14
+--
+-- >>> evalStr "2+3*"
+-- Nothing
+evalStr :: String -> Maybe Integer
+evalStr strExp = parseExp Lit Add Mul strExp >>= \ex -> pure (eval ex)
