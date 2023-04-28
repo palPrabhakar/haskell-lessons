0c1915f61645ab77d0b8b496d1569e37fe3464dc
Prabhakar Pal <prabhakar.pal00@gmail.com>
2023-04-27 21:45:04 -0400

    added stream data type

---
+++ hw6/Ex6.hs
@@ -0,0 +1,19 @@
+module Ex6 where
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+  show x = show (take 10 (streamToList x))
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f s = Cons s (streamFromSeed f (f s))
+
