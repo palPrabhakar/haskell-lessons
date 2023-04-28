8f2fa7bb5f8c6931066d758c421989ccf8a898f4
Prabhakar Pal <prabhakar.pal00@gmail.com>
2023-04-27 21:47:10 -0400

    added a stream of natural numbers

--- hw6/Ex6.hs
+++ hw6/Ex6.hs
@@ -17,3 +17,5 @@ streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
 streamFromSeed :: (a -> a) -> a -> Stream a
 streamFromSeed f s = Cons s (streamFromSeed f (f s))
 
+nats :: Stream Integer
+nats = streamFromSeed (1+) 0
